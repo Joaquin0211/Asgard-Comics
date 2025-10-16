d934bd989e35f3e36f3263c700b4d91a8936c654
Joaquin <j.olmedo021105@gmail.com>
2025-10-16 00:42:53 -0300

    Inclusion de la pagina principal del sitio

--- Frontend/src/views/Home.jsx
+++ Frontend/src/views/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import ComicList from '../components/ComicList';
+import WeeklyNews from '../components/sections/WeeklyNews';
+import Preorders from '../components/sections/Preorders';
+import Figures from '../components/sections/Figures';
+import Promotions from '../components/sections/Promotions';
+import Comics from '../components/sections/Comics';
+import TradingCards from '../components/sections/TradingCards';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to the Comic Store</h1>
-            <p>Explore our collection of comics below:</p>
-            <ComicList />
+        <div className="home-container">
+            <header className="hero-section">
+                <h1>Bienvenidos a Asgard Comics</h1>
+                <p>Tu destino para comics, mangas, figuras y más</p>
+            </header>
+
+            <main>
+                <WeeklyNews />
+                <Promotions />
+                <Comics />
+                <Preorders />
+                <Figures />
+                <TradingCards />
+            </main>
         </div>
     );
 };
