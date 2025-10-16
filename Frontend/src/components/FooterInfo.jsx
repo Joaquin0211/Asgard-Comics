6dcfb40ff6718cc695c05a7b127f44d4eb2b0528
Joaquin <j.olmedo021105@gmail.com>
2025-10-16 18:03:15 -0300

    Actualizacion del navbar

--- Frontend/src/components/FooterInfo.jsx
+++ Frontend/src/components/FooterInfo.jsx
@@ -3,42 +3,42 @@ import { Facebook, Instagram } from "react-feather";
 import "./FooterInfo.css";
 
 const paymentLogos = [
-  "visa.png", "mastercard.png", "american.png", "banelco.png", "cabal.png", "link.png", "naranja.png", "shopping.png", "dinero.png", "nativa.png", "argencard.png", "cabal2.png", "pagofacil.png", "rapipago.png", "visa2.png", "mastercard2.png", "mastercard3.png"
+"visa.png", "mastercard.png", "american.png", "banelco.png", "cabal.png", "link.png", "naranja.png", "shopping.png", "dinero.png", "nativa.png", "argencard.png", "cabal2.png", "pagofacil.png", "rapipago.png", "visa2.png", "mastercard2.png", "mastercard3.png"
 ];
 const shippingLogos = [
-  "envio.png", "andreani.png", "correo.png"
+"envio.png", "andreani.png", "correo.png"
 ];
 
 const FooterInfo = () => (
-  <div className="ac-footer-info">
+<div className="ac-footer-info">
     <div className="ac-footer-info__container">
-      <div className="ac-footer-info__contact">
+    <div className="ac-footer-info__contact">
         <span>¡Escribinos!</span>
         <div className="ac-footer-info__social">
-          <a href="#" aria-label="Instagram"><Instagram size={22} /></a>
-          <a href="#" aria-label="Facebook"><Facebook size={22} /></a>
+        <a href="#" aria-label="Instagram"><Instagram size={22} /></a>
+        <a href="#" aria-label="Facebook"><Facebook size={22} /></a>
         </div>
-      </div>
-      <div className="ac-footer-info__details">
+    </div>
+    <div className="ac-footer-info__details">
         <div className="ac-footer-info__payments">
-          <span className="ac-footer-info__label">Medios de pago</span>
-          <div className="ac-footer-info__logos">
+        <span className="ac-footer-info__label">Medios de pago</span>
+        <div className="ac-footer-info__logos">
             {paymentLogos.map((logo, i) => (
-              <img src={`/logos/${logo}`} alt="Medio de pago" key={i} />
+            <img src={`/logos/${logo}`} alt="Medio de pago" key={i} />
             ))}
-          </div>
+        </div>
         </div>
         <div className="ac-footer-info__shipping">
-          <span className="ac-footer-info__label">Medios de envío</span>
-          <div className="ac-footer-info__logos">
+        <span className="ac-footer-info__label">Medios de envío</span>
+        <div className="ac-footer-info__logos">
             {shippingLogos.map((logo, i) => (
-              <img src={`/logos/${logo}`} alt="Medio de envío" key={i} />
+            <img src={`/logos/${logo}`} alt="Medio de envío" key={i} />
             ))}
-          </div>
         </div>
-      </div>
+        </div>
     </div>
-  </div>
+    </div>
+</div>
 );
 
 export default FooterInfo;
